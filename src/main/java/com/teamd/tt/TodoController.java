0bac92888811dbe593e0b81a581e7a2966e0770e
Administrator <tncjs3598@naver.com>
2023-06-29 09:30:10 +0900

    진행중

--- src/main/java/com/teamd/tt/TodoController.java
+++ src/main/java/com/teamd/tt/TodoController.java
@@ -1,12 +1,10 @@
 package com.teamd.tt;
 
+import com.teamd.tt.model.SelRegion;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,13 +20,7 @@ public class TodoController {
 
     @GetMapping("/todo")
     @Operation(summary = "지역 목록")
-    public List<String> getRegion() {
-        return service.selRegion();
-    }
-
-    @GetMapping("/todo")
-    @Operation(summary = "세부 지역 목록")
-    public List<String> selDetailRegion(@RequestParam int idRegion) {
-        return service.selDetailRegion(idRegion);
+    public List<String> getRegion(@RequestBody SelRegion idRegion) {
+        return service.selRegion(idRegion);
     }
 }
--- src/main/java/com/teamd/tt/TodoService.java
+++ src/main/java/com/teamd/tt/TodoService.java
@@ -1,5 +1,6 @@
 package com.teamd.tt;
 
+import com.teamd.tt.model.SelRegion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +15,13 @@ public class TodoService {
         this.mapper = mapper;
     }
 
-    public List<String> selRegion() {
-        return mapper.selRegion();
+    public List<String> selRegion(SelRegion idregion) {
+        if(idregion.getIdRegion()) {
+            return mapper.selRegion();
+        } else if (idregion.getIdRegion() > 0) {
+            return mapper.selDetailRegion(idregion.getIdRegion());
+        }
+        return null;
     }
 
     public List<String> selDetailRegion(int idRegion) {
