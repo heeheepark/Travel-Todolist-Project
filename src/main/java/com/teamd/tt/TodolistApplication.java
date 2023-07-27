67c688cfc9ebc0eea7aeacbea5bbc2a8cee4fd6a
박주희(jooheepark) <pjh9641@gmail.com>
2023-07-27 19:07:14 +0900

    Update TodolistApplication.java

--- src/main/java/com/teamd/tt/TodolistApplication.java
+++ src/main/java/com/teamd/tt/TodolistApplication.java
@@ -2,12 +2,28 @@ package com.teamd.tt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST…)
+						.allowedOrigins("https://web-travel-todo-rt92alkkyf40y.sel4.cloudtype.app");
+			}
+		};
 	}
 
 }
